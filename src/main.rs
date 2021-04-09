ebc231cd75395c2d892034fe80a591bdf0b28d19
Sergey Mechtaev <mechtaev@gmail.com>
2021-04-09 11:29:58 +0100

    initial Dockerfile parser

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
+// This file is part of Modus.
+
+// Modus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Modus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Modus.  If not, see <https://www.gnu.org/licenses/>.
+
+#[macro_use]
+extern crate fp_core;
+
 use clap::{
     crate_version,
     App,
     Arg
 };
+use std::fs;
+
+mod dockerfile;
+
+use dockerfile::Dockerfile;
 
 
 fn main() {
     let matches = App::new("modus-transpile")
         .version(crate_version!())
+        .about("Converts Modusfile to Dockerfile")
         .arg(Arg::with_name("FILE")
             .required(true)
             .help("Sets the input Modusfile")
@@ -34,4 +58,8 @@ fn main() {
         println!("Value for query: {}", q);
     }
 
+    let input_file = matches.value_of("FILE").unwrap();
+    let content = fs::read_to_string(input_file).unwrap();
+    let df: Dockerfile = content.parse().unwrap();
+    println!("{}", df);
 }
