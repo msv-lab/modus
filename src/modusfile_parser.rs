4fdfb4977a3e1a2cb26a2d18d789adb929a1b122
Sergey Mechtaev <mechtaev@gmail.com>
2021-04-28 15:06:06 +0100

    polymorphic variants for instructions

--- src/modusfile_parser.rs
+++ src/modusfile_parser.rs
@@ -14,3 +14,8 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Modus.  If not, see <https://www.gnu.org/licenses/>.
+
+use crate::dockerfile_parser;
+use crate::datalog_parser;
+
+
