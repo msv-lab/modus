e50af3806805ac23e3700ed54f3a98e494da5839
Mao <m@maowtm.org>
2021-10-31 16:17:23 +0000

    Builtin: put string_concat into module, add unit test.

--- src/builtin.rs
+++ src/builtin.rs
@@ -15,8 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Modus.  If not, see <https://www.gnu.org/licenses/>.
 
-use crate::logic::{Atom, Literal, Term};
-use crate::sld::Variable;
+use crate::logic::{Literal, Term};
 
 pub trait BuiltinPredicate<C, V> {
     fn name(&self) -> &'static str;
@@ -62,77 +61,82 @@ impl<C: ToString, V> MaybeStringConst for Term<C, V> {
     }
 }
 
-fn string_concat_result<C: From<String>, V>(
-    a: String,
-    b: String,
-    c: String,
-) -> Option<Literal<C, V>> {
-    Some(Literal {
-        atom: Atom("string_concat".to_owned()),
-        args: vec![
-            Term::Constant(C::from(a)),
-            Term::Constant(C::from(b)),
-            Term::Constant(C::from(c)),
-        ],
-    })
-}
+mod string_concat {
+    use super::{BuiltinPredicate, MaybeStringConst};
+    use crate::logic::{Atom, Literal, Term};
 
-pub struct StringConcat1;
-impl<C: ToString + From<String>, V> BuiltinPredicate<C, V> for StringConcat1 {
-    fn name(&self) -> &'static str {
-        "string_concat"
+    fn string_concat_result<C: From<String>, V>(
+        a: String,
+        b: String,
+        c: String,
+    ) -> Option<Literal<C, V>> {
+        Some(Literal {
+            atom: Atom("string_concat".to_owned()),
+            args: vec![
+                Term::Constant(C::from(a)),
+                Term::Constant(C::from(b)),
+                Term::Constant(C::from(c)),
+            ],
+        })
     }
 
-    fn arg_groundness(&self) -> &'static [bool] {
-        &[false, false, true]
-    }
+    pub struct StringConcat1;
+    impl<C: ToString + From<String>, V> BuiltinPredicate<C, V> for StringConcat1 {
+        fn name(&self) -> &'static str {
+            "string_concat"
+        }
 
-    fn apply(&self, lit: &Literal<C, V>) -> Option<Literal<C, V>> {
-        let a = lit.args[0].as_str_const()?;
-        let b = lit.args[1].as_str_const()?;
-        let c = a.clone() + &b;
-        string_concat_result(a, b, c)
-    }
-}
+        fn arg_groundness(&self) -> &'static [bool] {
+            &[false, false, true]
+        }
 
-pub struct StringConcat2;
-impl<C: ToString + From<String>, V> BuiltinPredicate<C, V> for StringConcat2 {
-    fn name(&self) -> &'static str {
-        "string_concat"
-    }
-
-    fn arg_groundness(&self) -> &'static [bool] {
-        &[true, false, false]
-    }
-
-    fn apply(&self, lit: &Literal<C, V>) -> Option<Literal<C, V>> {
-        let b = lit.args[1].as_str_const()?;
-        let c = lit.args[2].as_str_const()?;
-        if let Some(a) = c.strip_suffix(&b) {
-            string_concat_result(a.to_string(), b, c)
-        } else {
-            None
+        fn apply(&self, lit: &Literal<C, V>) -> Option<Literal<C, V>> {
+            let a = lit.args[0].as_str_const()?;
+            let b = lit.args[1].as_str_const()?;
+            let c = a.clone() + &b;
+            string_concat_result(a, b, c)
         }
     }
-}
 
-pub struct StringConcat3;
-impl<C: ToString + From<String>, V> BuiltinPredicate<C, V> for StringConcat3 {
-    fn name(&self) -> &'static str {
-        "string_concat"
+    pub struct StringConcat2;
+    impl<C: ToString + From<String>, V> BuiltinPredicate<C, V> for StringConcat2 {
+        fn name(&self) -> &'static str {
+            "string_concat"
+        }
+
+        fn arg_groundness(&self) -> &'static [bool] {
+            &[true, false, false]
+        }
+
+        fn apply(&self, lit: &Literal<C, V>) -> Option<Literal<C, V>> {
+            let b = lit.args[1].as_str_const()?;
+            let c = lit.args[2].as_str_const()?;
+            if let Some(a) = c.strip_suffix(&b) {
+                string_concat_result(a.to_string(), b, c)
+            } else {
+                None
+            }
+        }
     }
 
-    fn arg_groundness(&self) -> &'static [bool] {
-        &[false, true, false]
-    }
+    pub struct StringConcat3;
+    impl<C: ToString + From<String>, V> BuiltinPredicate<C, V> for StringConcat3 {
+        fn name(&self) -> &'static str {
+            "string_concat"
+        }
 
-    fn apply(&self, lit: &Literal<C, V>) -> Option<Literal<C, V>> {
-        let a = lit.args[0].as_str_const()?;
-        let c = lit.args[2].as_str_const()?;
-        if let Some(b) = c.strip_prefix(&a) {
-            string_concat_result(a, b.to_string(), c)
-        } else {
-            None
+        fn arg_groundness(&self) -> &'static [bool] {
+            &[false, true, false]
+        }
+
+        fn apply(&self, lit: &Literal<C, V>) -> Option<Literal<C, V>> {
+            let a = lit.args[0].as_str_const()?;
+            let c = lit.args[2].as_str_const()?;
+            if let Some(b) = c.strip_prefix(&a) {
+                string_concat_result(a, b.to_string(), c)
+            } else {
+                None
+            }
         }
     }
 }
@@ -210,11 +214,96 @@ where
 {
     select_builtins!(
         lit,
-        StringConcat1,
-        StringConcat2,
-        StringConcat3,
+        string_concat::StringConcat1,
+        string_concat::StringConcat2,
+        string_concat::StringConcat3,
         run::Run,
         from::From
     );
     None
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    pub fn test_select() {
+        use crate::logic::toy::{Clause, Literal, Term, Variable};
+        use crate::logic::Atom;
+
+        let lit = Literal {
+            atom: Atom("run".to_owned()),
+            args: vec![Term::Constant(Atom("hello".to_owned()))],
+        };
+        let b = super::select_builtin(&lit);
+        assert!(b.is_some());
+        let b = b.unwrap();
+        assert_eq!(b.name(), "run");
+        assert_eq!(b.apply(&lit), Some(lit));
+
+        let lit = Literal {
+            atom: Atom("string_concat".to_owned()),
+            args: vec![
+                Term::Constant(Atom("hello".to_owned())),
+                Term::Constant(Atom("world".to_owned())),
+                Term::Variable("X".to_owned()),
+            ],
+        };
+        let b = super::select_builtin(&lit);
+        assert!(b.is_some());
+        let b = b.unwrap();
+        assert_eq!(b.name(), "string_concat");
+        assert_eq!(
+            b.apply(&lit),
+            Some(Literal {
+                atom: Atom("string_concat".to_owned()),
+                args: vec![
+                    Term::Constant(Atom("hello".to_owned())),
+                    Term::Constant(Atom("world".to_owned())),
+                    Term::Constant(Atom("helloworld".to_owned())),
+                ]
+            })
+        );
+
+        let lit = Literal {
+            atom: Atom("xxx".to_owned()),
+            args: vec![Term::Constant(Atom("hello".to_owned()))],
+        };
+        let b = super::select_builtin(&lit);
+        assert!(b.is_none());
+    }
+
+    #[test]
+    pub fn test_from_run() {
+        use crate::logic::Atom;
+        use crate::modusfile::{Constant as C, Literal, Term, Variable as V};
+        type Clause = crate::logic::Clause<C, V>;
+
+        let rules = vec![Clause {
+            head: Literal {
+                atom: Atom("a".to_owned()),
+                args: vec![],
+            },
+            body: vec![
+                Literal {
+                    atom: Atom("from".to_owned()),
+                    args: vec![Term::Constant(C::String("ubuntu".to_owned()))],
+                },
+                Literal {
+                    atom: Atom("run".to_owned()),
+                    args: vec![Term::Constant(C::String("rm -rf /".to_owned()))],
+                },
+            ],
+        }];
+        let goals = vec![Literal {
+            atom: Atom("a".to_owned()),
+            args: vec![],
+        }];
+        let tree = crate::sld::sld(&rules, &goals, 100).unwrap();
+        let solutions = crate::sld::solutions(&tree);
+        assert_eq!(solutions.len(), 1);
+        assert!(solutions.contains(&goals));
+        let proof = crate::sld::proofs(&tree, &rules, &goals);
+        assert_eq!(proof.len(), 1);
+        println!("{:?}", proof[0]);
+    }
 }
