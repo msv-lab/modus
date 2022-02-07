a07a44ecb245b00eb987d7312b6ccf63681dfa34
Mao <m@maowtm.org>
2022-02-07 18:26:25 +0000

    Add several tests

--- test/test_merge.py
+++ test/test_merge.py
@@ -22,8 +22,6 @@ class TestMerge(ModusTestCase):
         self.assertEqual(img.read_file("/tmp/file2"), "ccc\n")
 
     def test_merge_local_copy(self):
-        # TODO: unimplemented
-        return
         self.init_files()
         mf = dedent("""\
             a :-
@@ -35,6 +33,43 @@ class TestMerge(ModusTestCase):
         imgs = self.build(mf, "a")
         img = imgs[Fact("a", ())]
         self.assertEqual(img.read_file("/tmp/file"), "content\nnewline\n")
+
+    def test_merge_local_copy_inworkdir(self):
+        self.init_files()
+        mf = dedent("""\
+            a :-
+                from("alpine"),
+                (
+                    copy("file", "file"),
+                    run("echo newline >> file")
+                )::in_workdir("/tmp")::merge.""")
+        imgs = self.build(mf, "a")
+        img = imgs[Fact("a", ())]
+        self.assertEqual(img.read_file("/tmp/file"), "content\nnewline\n")
+
+    def test_merge_single_copy_inworkdir(self):
+        md = dedent("""\
+            a :- from("alpine")::set_workdir("/tmp/aaa"), run("echo aaaaa > /tmp/aaa/file").
+            b :- from("alpine"), (
+                a::copy("file", "aaa"),
+                run("echo bbbbb >> aaa")
+            )::in_workdir("/tmp")::merge.
+        """)
+        imgs = self.build(md, "b")
+        img = imgs[Fact("b", ())]
+        self.assertEqual(img.read_file("/tmp/aaa"), "aaaaa\nbbbbb\n")
+
+    def test_merge_single_copy_setworkdir(self):
+        md = dedent("""\
+            a :- from("alpine")::set_workdir("/tmp/aaa"), run("echo aaaaa > /tmp/aaa/file").
+            b :- from("alpine")::set_workdir("/tmp"), (
+                a::copy("file", "aaa"),
+                run("echo bbbbb >> aaa")
+            )::merge.
+        """)
+        imgs = self.build(md, "b")
+        img = imgs[Fact("b", ())]
+        self.assertEqual(img.read_file("/tmp/aaa"), "aaaaa\nbbbbb\n")
 
     def test_merge_multi_copy(self):
         self.init_files()
@@ -55,3 +90,25 @@ class TestMerge(ModusTestCase):
         img = imgs[Fact("a", ())]
         self.assertEqual(img.read_file("/tmp/file1"), "aaa\n")
         self.assertEqual(img.read_file("/tmp/file2"), "bbb\n")
+
+    def test_workdir_interaction(self):
+        md = dedent("""\
+            a :- from("alpine")::set_workdir("/tmp"), (
+                run("pwd >> /log"),
+                run("pwd >> /log")::in_workdir("a"),
+                (
+                    run("pwd >> /log"),
+                    run("pwd >> /log")::in_workdir("c"),
+                    run("pwd >> /log")::in_workdir("/")
+                )::in_workdir("b")
+            )::merge.""")
+
+        imgs = self.build(md, "a")
+        img = imgs[Fact("a", ())]
+        self.assertEqual(img.read_file("/log"), dedent("""\
+            /tmp
+            /tmp/a
+            /tmp/b
+            /tmp/b/c
+            /
+        """))
