c3f252cdae6761116a96db5d5066a51d5959e4df
Mao <m@maowtm.org>
2022-03-07 18:44:34 +0000

    Delete test_3 in test_simple
    We do not allow ::in_workdir to operate on images directly.

--- test/test_simple.py
+++ test/test_simple.py
@@ -57,15 +57,6 @@ class TestSimple(ModusTestCase):
         first_img = imgs[Fact("a", ())]
         self.assertEqual(first_img.read_file("/tmp/new_dir/a"), "aaa\n")
 
-    def test_3(self):
-        mf = dedent("""\
-        a :- (from("alpine")::set_workdir("/tmp/new_dir"),
-                run("echo aaa > a"))::in_workdir("bbb").""")
-        imgs = self.build(mf, "a")
-        self.assertEqual(len(imgs), 1)
-        first_img = imgs[Fact("a", ())]
-        self.assertEqual(first_img.read_file("/tmp/new_dir/bbb/a"), "aaa\n")
-
     def test_4(self):
         mf = dedent("""\
         a :- from("alpine")::set_workdir("/tmp/new_dir"),
