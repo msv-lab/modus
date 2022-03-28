36510ab881a9c6137dd5bdb78599d120d724cc91
Mao <m@maowtm.org>
2022-03-28 15:18:46 +0100

    Fix grammar in cargo-publish.sh

--- cargo-publish.sh
+++ cargo-publish.sh
@@ -5,7 +5,7 @@ set -xe
 COMMIT_HASH=$(git rev-parse HEAD)
 SHORT_HASH=${COMMIT_HASH:0:10}
 if ! docker manifest inspect "ghcr.io/modus-continens/modus-buildkit-frontend:$SHORT_HASH"; then
-  echo "Frontend image does not exist on ghcr - did the CI run completed successfully?"
+  echo "Frontend image does not exist on ghcr - did the CI run complete successfully?"
   exit 1
 fi
 
